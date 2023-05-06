ce785c6373d0e58bfa828dd7c24f8dae6181ca92
EduardoBlackF <eduardo.black@tecsup.edu.pe>
2023-05-06 00:58:12 -0500

    Agregar archivos de proyecto.

---
+++ App6/App6/Models/UserM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App6.Models
+{
+    public class UserM
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public UserM(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+    }
+
+    public class UserGroupM : List<UserM>
+    {
+        public string GroupName { get; private set; }
+
+        public UserGroupM(string groupName, List<UserM> source) : base(source)
+        {
+            GroupName = groupName;
+        }
+    }
+
+
+}
---
+++ App6/App6/ViewModels/UserVM.cs
@@ -0,0 +1,43 @@
+﻿using App6.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace App6.ViewModels
+{
+    public class UserVM
+    {
+        public ObservableCollection<UserGroupM> UserList { get; set; }
+
+        public UserVM() 
+        {
+            this.UserList = new ObservableCollection<UserGroupM>();
+            var group1 = new List<UserM>
+            {
+                new UserM("Eduardo", "Black", 24),
+                new UserM("Enrique", "Ferruzo", 32),
+                new UserM("Carlos", "Carranza", 18),
+            };
+
+            UserList.Add(new UserGroupM("Desarrollo de Software", group1));
+
+            var group2 = new List<UserM>
+            {
+                new UserM("Jaime", "Torres", 23),
+                new UserM("Pablo", "Perez", 17),
+            };
+
+            UserList.Add(new UserGroupM("Desarrollo de Videojuegos", group2));
+
+            var group3 = new List<UserM>
+            {
+                new UserM("Fabrizio", "Porras", 25),
+                new UserM("Fernando", "Guerra", 24),
+                new UserM("Elena", "Torres", 24),
+            };
+
+            UserList.Add(new UserGroupM("Diseño Industrial", group3));
+        }
+    }
+}
